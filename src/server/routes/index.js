62a521dee88aa986be6b4f75b3440df5c02622ab
Kazuyuki Matsuo <matu.git@gmail.com>
2016-09-08 16:42:32 +0900

    LEDチカを基盤に用意して、/led の uri でその制御ができるかどうかのテスト実装

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var gpio = require('../rc/gpio');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/led', function(req, res) {
+  console.log(req.query.action);
+  if (req.query.action == 'on') {
+    gpio.setValue(24, 1);
+  } else if (req.query.action == 'off') {
+    gpio.setValue(24, 0);
+  } else if (req.query.action == 'get') {
+    console.log(gpio.getValue(24));
+  }
 });
 
 module.exports = router;
