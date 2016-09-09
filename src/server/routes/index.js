39f7dc744fd466db75d4a022bc2488bcc56b57a0
Kazuyuki Matsuo <matu.git@gmail.com>
2016-09-09 11:31:04 +0900

    クライアント側からのAPI呼び出しを post にしたので get -> post に変更

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -7,15 +7,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/led', function(req, res) {
-  console.log(req.query.action);
-  if (req.query.action == 'on') {
+router.post('/led', function(req, res) {
+  console.log(req.body.action);
+  if (req.body.action == 'on') {
     gpio.setValue(24, 1);
-  } else if (req.query.action == 'off') {
+  } else if (req.body.action == 'off') {
     gpio.setValue(24, 0);
-  } else if (req.query.action == 'get') {
+  } else if (req.body.action == 'get') {
     console.log(gpio.getValue(24));
   }
+  res.send(req.body);
 });
 
 module.exports = router;
