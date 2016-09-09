d78c0b0eb7427d69df9afc3bad23ea1dc20de1d4
Kazuyuki Matsuo <matu.git@gmail.com>
2016-09-09 11:31:40 +0900

    クライアントの js が増えたので　deploy プロセスで js のコピー処理追加

--- gulpfile.js
+++ gulpfile.js
@@ -25,8 +25,22 @@ gulp.task('init-vendors:bootstrap:js', () => {
     .pipe(gulp.dest('./public/vendors/js'))
 });
 
-gulp.task('clean', ['clean:public']);
+gulp.task('clean', ['clean:public:vendors', 'clean:public:app']);
 
-gulp.task('clean:public', function () {
-  return del("public");
+gulp.task('clean:public:vendors', function () {
+  return del("public/vendors");
 });
+
+gulp.task('clean:public:app', function () {
+  return del("public/app");
+});
+
+gulp.task('build:clean', ['clean:public:app']);
+
+gulp.task('build:deploy', ['build:client:js']);
+
+gulp.task('build:client:js', () => {
+  return gulp.src(['./src/client/js/*'])
+    .pipe(gulp.dest('./public/app/js'))
+});
+
